636958d4e10ca3b7880230f2c36cb6e289537ea0
Y_Kashiwagi <Y_Kashiwagi@LAPTOP-UAB3AID9>
2025-06-10 12:14:26 +0900

    ユニコーーーーーーーーーーン！

---
+++ src/main/Insert.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/main/insert"})
+public class Insert extends HttpServlet {
+
+	protected void post(HttpServletRequest req, HttpServletResponse resp
+			) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		  req.setCharacterEncoding("UTF-8");
+
+	}
+}
