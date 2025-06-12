4d1c1bd5dc4f3f12b8f93abbe60139f0a6f8aa4e
s_saitou <s_saitou@LAPTOP-T4PL1RQN>
2025-06-12 12:59:43 +0900

    Bean間違ってんだよ(# ﾟДﾟ)

--- src/bean/Class_Num.java
+++ src/bean/Class_Num.java
@@ -2,26 +2,26 @@ package bean;
 
 public class Class_Num implements java.io.Serializable {
 
-    private String schoolCd;
-    private String classNum;
+    private int schoolCd;
+    private int classNum;
 
     // 学校コードの取得
-    public String getSchoolCd() {
+    public int getSchoolCd() {
         return schoolCd;
     }
 
     // 学校コードの設定
-    public void setSchoolCd(String schoolCd) {
+    public void setSchoolCd(int schoolCd) {
         this.schoolCd = schoolCd;
     }
 
     // クラス名の取得
-    public String getClassNum() {
+    public int getClassNum() {
         return classNum;
     }
 
     // クラス名の設定
-    public void setClassNum(String classNum) {
+    public void setClassNum(int classNum) {
         this.classNum = classNum;
     }
 }
