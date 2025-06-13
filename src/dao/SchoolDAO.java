4a250390a6ded9298c0bbe01087d82ed43d55c5d
s_saitou <s_saitou@LAPTOP-T4PL1RQN>
2025-06-13 13:58:21 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/KashiwagiYukito/JavaSD_TeamB.git
    
    Conflicts:
    	src/bean/Class_Num.java

--- src/dao/SchoolDAO.java
+++ src/dao/SchoolDAO.java
@@ -1,5 +1,30 @@
 package dao;
 
-public class SchoolDAO extends DAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import bean.School;
+
+public class SchoolDAO extends DAO {
+	public List<School> findByCD(int CD) throws Exception {
+		List<School> list = new ArrayList<>();
+	
+	try (Connection con = getConnection()) {
+		String sql = "SELECT * FROM school WHERE CD = ? ORDER BY name DESC";
+	PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, CD);
+		ResultSet rs = st.executeQuery();
+	
+		while (rs.next()) {
+			School order = new School();
+		    order.setCd(rs.getString("CD"));
+		    order.setName(rs.getString("NAME"));
+		    list.add(order);
+		}
+	}
+	return list;
+	}
 }
--- src/dao/StudentDAO.java
+++ src/dao/StudentDAO.java
@@ -1,6 +1,30 @@
 package dao;
 
-public class StudentDAO extends DAO {
-	
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import bean.Student;
+
+public class StudentDAO extends DAO {
+	public List<Student> findByStudent(int ) throws Exception {
+		List<Student> list = new ArrayList<>();
+	
+	try (Connection con = getConnection()) {
+		String sql = "SELECT * FROM  WHERE  = ? ORDER BY  DESC";
+	PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1,);
+		ResultSet rs = st.executeQuery();
+	
+		while (rs.next()) {
+			School order = new School();
+		    order.set(rs.get(""));
+		    order.set(rs.get(""));
+		    list.add(order);
+		}
+	}
+	return list;
+	}
 }
